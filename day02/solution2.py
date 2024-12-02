d7f1d43ae85c4c115a951b30775f0f61d77b14c0
Daniel Bjerkehagen <daniel.bjerkehagen@sintef.no>
2024-12-02 14:03:44 +0100

    Document insight into part2 solution

--- day02/solution2.py
+++ day02/solution2.py
@@ -3,11 +3,25 @@ with open("day02\input1.txt", "r") as fp:
     for report in fp:
         levels = [int(l) for l in report.split(" ")]
         increases = [levels[i] - levels[i - 1] for i in range(1, len(levels))]
-        signs = [1 if l >= 0 else -1 for l in increases]
-        if not sum(signs) >= len(increases) - 1 or not sum(signs) <= len(increases) + 1:
-            continue
-        func = (lambda i: 4 > i >= 1) if all(positive) else (lambda i: -4 < i <= -1)
-        if not all([func(i) for i in increases]):
+        same_signs = [i >= 0 for i in increases]
+        if sum(same_signs) < len(same_signs) // 2:
+            same_signs = [not s for s in same_signs]  # Decreasing
+        small = [4 > abs(i) >= 1 for i in increases]
+        crosscheck = [one and two for one, two in zip(same_signs, small)]
+
+        num_unsafe = len(crosscheck) - sum(crosscheck)
+        if num_unsafe > 1:
             continue
         num_safe += 1
+        # ! If the single problem is of too high increase, since we at this point know that only one
+        # level is a problem, then we can safely remove the level to the left of too high increase without
+        # causing further magnitude errors.
+        # TODO: But this can create increase errors?
+
+        # Similarily, if the problem is a single wrong sign, we can safely remove the level to the right
+        # of the increase, since we know the one to the right of that again at most is -1 + 3 away from the level
+        # left of the increase, thus not creating more magnitude errors.
+        # TODO: But this can create increase errors?
+
+
 print(num_safe)
