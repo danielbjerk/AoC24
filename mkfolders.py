43986a2b54ed62f98b804a3c5a220def9dac5a89
Daniel Bjerkehagen <daniel.bjerkehagen@sintef.no>
2024-12-02 11:42:41 +0100

    Fix py input

--- mkfolders.py
+++ mkfolders.py
@@ -3,5 +3,5 @@ from pathlib import Path
 for i in range(1, 24 + 1):
     top = Path(f"day{i:02}")
     top.mkdir()
-    (top / "input.py").touch()
+    (top / "input.txt").touch()
     (top / "solution.py").touch()
